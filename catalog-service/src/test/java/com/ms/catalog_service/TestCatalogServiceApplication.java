5ee97699d8b96776662d6b766a0919ff631725c9
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-18 01:16:27 +0530

    added catalog service module

--- catalog-service/src/test/java/com/ms/catalog_service/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/ms/catalog_service/TestCatalogServiceApplication.java
@@ -1,14 +1,11 @@
 package com.ms.catalog_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 public class TestCatalogServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
