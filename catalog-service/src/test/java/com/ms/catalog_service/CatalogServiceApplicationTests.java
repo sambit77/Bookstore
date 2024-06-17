0bb9a3c7d80ad13b518c6fae026cd5f6a20723d0
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-18 00:33:16 +0530

    added actuators/info

---
+++ catalog-service/src/test/java/com/ms/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ms.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
