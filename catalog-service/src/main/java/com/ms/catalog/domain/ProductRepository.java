a08707eacfbabf4a9fa4e7e4eff6be4850c7fce6
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-19 04:17:45 +0530

    added catalog api implementation

--- catalog-service/src/main/java/com/ms/catalog/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/ms/catalog/domain/ProductRepository.java
@@ -4,6 +4,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
     Optional<ProductEntity> findByCode(String code);
 }
