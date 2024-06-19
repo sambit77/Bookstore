120fffb1b8522a9567fbed26258c09f917c3aff3
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-19 21:02:55 +0530

    dockerized catalog-service

--- catalog-service/src/main/java/com/ms/catalog/domain/Product.java
+++ catalog-service/src/main/java/com/ms/catalog/domain/Product.java
@@ -2,4 +2,4 @@ package com.ms.catalog.domain;
 
 import java.math.BigDecimal;
 
-public record Product(String code, String name, String description, String imageUrl, BigDecimal price) { }
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price) {}
