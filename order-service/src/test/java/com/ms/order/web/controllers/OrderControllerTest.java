22b847615ed9a40bdd022c1127d972bd86fb25de
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-20 21:25:13 +0530

    added unit & int tests for create-order api

---
+++ order-service/src/test/java/com/ms/order/web/controllers/OrderControllerTest.java
@@ -0,0 +1,67 @@
+package com.ms.order.web.controllers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.ms.order.AbstractIntegrationTest;
+import com.ms.order.testdata.TestDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class OrderControllerTest extends AbstractIntegrationTest {
+
+    @Nested
+    class CreateOrderTests {
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            // mockGetProductByCode("P100", "Product 1", new BigDecimal("25.50"));
+            var payload =
+                    """
+                        {
+                            "customer" : {
+                                "name": "Siva",
+                                "email": "siva@gmail.com",
+                                "phone": "999999999"
+                            },
+                            "deliveryAddress" : {
+                                "addressLine1": "HNO 123",
+                                "addressLine2": "Kukatpally",
+                                "city": "Hyderabad",
+                                "state": "Telangana",
+                                "zipCode": "500072",
+                                "country": "India"
+                            },
+                            "items": [
+                                {
+                                    "code": "P100",
+                                    "name": "Product 1",
+                                    "price": 25.50,
+                                    "quantity": 1
+                                }
+                            ]
+                        }
+                    """;
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
+
+        @Test
+        void shouldReturnBadRequestWhenMandatoryDataIsMissing() {
+            var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
+    }
+}
---
+++ order-service/src/test/java/com/ms/order/web/controllers/OrderControllerUnitTests.java
@@ -0,0 +1,65 @@
+package com.ms.order.web.controllers;
+
+import static com.ms.order.testdata.TestDataFactory.*;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ms.order.domain.OrderService;
+import com.ms.order.domain.SecurityService;
+import com.ms.order.domain.models.CreateOrderRequest;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerUnitTests {
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private SecurityService securityService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        given(securityService.getLoginUserName()).willReturn("siva");
+    }
+
+    @ParameterizedTest(name = "[{index}]-{0}")
+    @MethodSource("createOrderRequestProvider")
+    void shouldReturnBadRequestWhenOrderPayloadIsInvalid(CreateOrderRequest request) throws Exception {
+        given(orderService.createOrder(eq("siva"), any(CreateOrderRequest.class)))
+                .willReturn(null);
+
+        mockMvc.perform(post("/api/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    static Stream<Arguments> createOrderRequestProvider() {
+        return Stream.of(
+                arguments(named("Order with Invalid Customer", createOrderRequestWithInvalidCustomer())),
+                arguments(named("Order with Invalid Delivery Address", createOrderRequestWithInvalidDeliveryAddress())),
+                arguments(named("Order with No Items", createOrderRequestWithNoItems())));
+    }
+}
