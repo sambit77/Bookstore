8d3fb4ff1d2a4a5095109389bfbe997883037b7e
Sambit Pradhan <sambitgulu@gmail.com>
2024-06-26 02:47:41 +0530

    added api-gateway

---
+++ api-gateway/src/test/java/com/ms/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ms.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
